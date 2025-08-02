2f9ed66340cc65c38d2d291c25fb5ddf2d3593c5
Mohammed Hafiz <mohammed.hafiz.2710@gmail.com>
2025-08-02 16:08:32 +0300

    Lastest transaction list done

--- lib/Features/homepage/presentation/views/widgets/lastest_transaction_list_view_builder.dart
+++ lib/Features/homepage/presentation/views/widgets/lastest_transaction_list_view_builder.dart
@@ -7,20 +7,29 @@ class LastestTransactionListViewBuilder extends StatelessWidget {
   const LastestTransactionListViewBuilder({super.key});
   static const List<UserInfoModel> userInfoList = [
     UserInfoModel(image: Assets.imagesSmileface, title: "Madrani Andi", subtitle: "Madraniadi20@gmail"),
-    UserInfoModel(image: Assets.imagesSmileface2, title: "Josua Nunito", subtitle: "Josh Nunito@gmail.com"),
+    UserInfoModel(image: Assets.imagesSmileface2, title: "Josua Nunito", subtitle: "Josh_Nunito@gmail.com"),
     UserInfoModel(image: Assets.imagesSmileface, title: "Madrani Andi", subtitle: "Madraniadi20@gmail"),
+    UserInfoModel(image: Assets.imagesSmileface2, title: "Josua Nunito", subtitle: "Josh_Nunito@gmail.com"),
   ];
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 80,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: userInfoList.length,
-        itemBuilder: (context, index) {
-          return IntrinsicWidth(child: UserInfo(userInfoModel: userInfoList[index]));
-        },
+    // Using SingleChildScrollView to allow horizontal scrolling 100% responsive but not recommended for performance if the list is large.
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: userInfoList.map((e) => IntrinsicWidth(child: UserInfo(userInfoModel: e))).toList(),
       ),
     );
+    // Alternatively, you can use ListView.builder for better performance with large lists.
+    // return SizedBox(
+    //   height: 80,
+    //   child: ListView.builder(
+    //     scrollDirection: Axis.horizontal,
+    //     itemCount: userInfoList.length,
+    //     itemBuilder: (context, index) {
+    //       return IntrinsicWidth(child: UserInfo(userInfoModel: userInfoList[index]));
+    //     },
+    //   ),
+    // );
   }
 }
